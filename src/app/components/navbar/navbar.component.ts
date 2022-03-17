46bc2254e1df1d13c8920aa8264f0c67573f58f4
moises56 <moiavil56@gmail.com>
2022-03-17 17:17:09 -0600

    first commit

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
